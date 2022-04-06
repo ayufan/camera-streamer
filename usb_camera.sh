568a44a37278c8e7ea36cf3ed4a3b7efaa6c99e6
Kamil Trzcinski <ayufan@ayufan.eu>
2022-04-06 13:29:11 +0200

    Fix script directory

--- usb_camera.sh
+++ usb_camera.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+cd "$SCRIPT_DIR"
+
 set -xeo pipefail
 make
 ./camera_stream -camera-path=$(echo /dev/v4l/by-id/usb-*-video-index0 ) "$@"
