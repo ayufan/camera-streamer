ayufan/camera-streamer

commit 3c818772d11ec85dd0c9a7da4cb8606898fd18a2

Add scripts to create build env for other architectures

device/libcamera/device.cc
@@ -123,7 +123,7 @@ void libcamera_device_close(device_t *dev)
 int libcamera_device_set_fps(device_t *dev, int desired_fps)
 {
   int64_t frame_time = 1000000 / desired_fps;
-  dev->libcamera->controls.set(libcamera::controls::FrameDurationLimits, { frame_time, frame_time });
+  dev->libcamera->controls.set(libcamera::controls::FrameDurationLimits.id(), libcamera::ControlValue(frame_time));
   return 0;
 }
 

.dockerignore
@@ -1,0 +1,9 @@
+*.o
+*.d
+*.a
+test_*
+tmp/
+.vscode/
+.git/
+CMakeFiles/
+examples/

Dockerfile
@@ -1,0 +1,10 @@
+ARG DOCKER_ARCH
+FROM ${DOCKER_ARCH}debian:bullseye as build_env
+
+# Default packages
+RUN apt-get -y update && apt-get -y install gnupg2 libavformat-dev libavutil-dev libavcodec-dev v4l-utils pkg-config xxd build-essential ccache
+
+# Add RPI packages
+RUN echo "deb http://archive.raspberrypi.org/debian/ bullseye main" > /etc/apt/sources.list.d/raspi.list
+RUN apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 82B129927FA3303E
+RUN apt-get -y update && apt-get -y install libcamera-dev liblivemedia-dev

build-env
@@ -1,0 +1,21 @@
+#!/bin/bash
+
+if [[ $# -gt 1 ]]; then
+  echo "usage: $0 <amd64|arm32v7|arm64v8>"
+  exit 1
+fi
+
+docker_arch=""
+docker_image="camera_streamer_build_env"
+[[ -n "$1" ]] && docker_arch="$1/" && docker_image="${build_env}:${1}"
+
+echo "$UID"
+
+PWD=$(pwd)
+ROOT=$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
+
+set -xeo pipefail
+
+docker build -t "$docker_image" --build-arg "DOCKER_ARCH=$docker_arch" --target build_env - < Dockerfile
+
+exec docker run --rm -it -u "$UID" -v "$ROOT" -w "$ROOT" "$docker_image"

commit 878ad95598a10e67f744666d4b66f741b49676f1

Fix bug introduced by https://github.com/ayufan-research/camera-streamer/commit/3c818772d11ec85dd0c9a7da4cb8606898fd18a2#diff-6eb6287b4cf8659acef0809a500f07e26feac08a34f12b17a8897127a5d84ab8

device/libcamera/device.cc
@@ -123,7 +123,7 @@ void libcamera_device_close(device_t *dev)
 int libcamera_device_set_fps(device_t *dev, int desired_fps)
 {
   int64_t frame_time = 1000000 / desired_fps;
-  dev->libcamera->controls.set(libcamera::controls::FrameDurationLimits.id(), libcamera::ControlValue(frame_time));
+  dev->libcamera->controls.set(libcamera::controls::FrameDurationLimits, { frame_time, frame_time });
   return 0;
 }
 