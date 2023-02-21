38c1811fdf1ffb96195844ed9d4395e8b50e3806
Kamil Trzcinski <ayufan@ayufan.eu>
2023-02-21 23:08:23 +0100

    tools: usb_camera.sh accepts `CAMERA_INDEX=0..n`

--- tools/usb_camera.sh
+++ tools/usb_camera.sh
@@ -3,6 +3,8 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd "$SCRIPT_DIR/.."
 
+CAMERA_PATH=( $(echo /dev/v4l/by-id/usb-*-video-index0) )
+
 set -xeo pipefail
 make -j$(nproc)
-$GDB ./camera-streamer -camera-path=$(echo /dev/v4l/by-id/usb-*-video-index0) "$@"
+$GDB ./camera-streamer -camera-path="${CAMERA_PATH[${CAMERA_INDEX:-0}]}" "$@"
