cdb62efd931b8bde5ab49d5319091714f48027b1
Kamil Trzcinski <ayufan@ayufan.eu>
2023-03-08 19:36:42 +0100

    tools: update dump cameras script to include libcamera devices

--- tools/dump_cameras.sh
+++ tools/dump_cameras.sh
@@ -2,11 +2,19 @@
 
 exec 2>&1
 
+prefix() {
+  while IFS= read -r LINE; do
+    printf "%s | %s\n" "$1" "$LINE"
+  done
+}
+
 ( set -x ; echo "$(cat /sys/firmware/devicetree/base/model | tr -d '\0')" )
 
 ( set -x ; uname -a )
 
-( set -x ; v4l2-ctl --list-devices )
+( set -x ; v4l2-ctl --list-devices ) | prefix "v4l2-ctl"
+
+( set -x ; libcamera-hello --list-cameras ) | prefix "libcamera"
 
 for device in /dev/video*; do
   echo "===================================="
@@ -17,8 +25,6 @@ for device in /dev/video*; do
     v4l2-ctl -d "$device" --info \
     --list-formats-ext --list-fields \
     --list-formats-out --list-fields-out
-  ) | while IFS= read -r LINE; do
-    printf "%s | %s\n" "$device" "$LINE"
-  done
+  ) | prefix "$device"
   echo
 done
