9224db147e84b7505e541703c6a28c715573e218
Kamil Trzcinski <ayufan@ayufan.eu>
2023-04-13 11:09:20 +0200

    Ensure that `/snapshot` is always "up-to date"

--- output/http_jpeg.c
+++ output/http_jpeg.c
@@ -3,8 +3,12 @@
 
 #include "output.h"
 #include "util/http/http.h"
+#include "util/opts/log.h"
 #include "device/buffer.h"
 #include "device/buffer_lock.h"
+
+#define SNAPSHOT_TIMEOUT_MS 3000
+#define SNAPSHOT_DEFAULT_DELAY_PARAM "300"
 
 #define PART_BOUNDARY "123456789000000000000987654321"
 #define CONTENT_TYPE "image/jpeg"
@@ -20,19 +24,42 @@ static const char *const STREAM_PART = "Content-Type: " CONTENT_TYPE "\r\n" CONT
 static const char *const STREAM_BOUNDARY = "\r\n"
                                            "--" PART_BOUNDARY "\r\n";
 
-int http_snapshot_buf_part(buffer_lock_t *buf_lock, buffer_t *buf, int frame, FILE *stream)
+typedef struct
 {
-  fprintf(stream, "HTTP/1.1 200 OK\r\n");
-  fprintf(stream, "Content-Type: image/jpeg\r\n");
-  fprintf(stream, "Content-Length: %zu\r\n", buf->used);
-  fprintf(stream, "\r\n");
-  fwrite(buf->start, buf->used, 1, stream);
+  FILE *stream;
+  uint64_t start_time_us;
+} http_snapshot_t;
+
+int http_snapshot_buf_part(buffer_lock_t *buf_lock, buffer_t *buf, int frame, http_snapshot_t *snapshot)
+{
+  // Ignore frames that are captured
+  if (buf->captured_time_us < snapshot->start_time_us) {
+    return 0;
+  }
+
+  fprintf(snapshot->stream, "HTTP/1.1 200 OK\r\n");
+  fprintf(snapshot->stream, "Content-Type: image/jpeg\r\n");
+  fprintf(snapshot->stream, "Content-Length: %zu\r\n", buf->used);
+  fprintf(snapshot->stream, "\r\n");
+  fwrite(buf->start, buf->used, 1, snapshot->stream);
   return 1;
 }
 
 void http_snapshot(http_worker_t *worker, FILE *stream)
 {
-  int n = buffer_lock_write_loop(&snapshot_lock, 1, 0, (buffer_write_fn)http_snapshot_buf_part, stream);
+  // passing the max_delay=0 will ensure that frame is capture at this exact moment
+  const char *max_delay = http_get_param(worker, "max_delay");
+  if (!max_delay) {
+    max_delay = SNAPSHOT_DEFAULT_DELAY_PARAM;
+  }
+
+  http_snapshot_t snapshot = {
+    .stream = stream,
+    .start_time_us = get_monotonic_time_us(NULL, NULL) - atoi(max_delay) * 1000
+  };
+
+  int n = buffer_lock_write_loop(&snapshot_lock, 1, SNAPSHOT_TIMEOUT_MS,
+    (buffer_write_fn)http_snapshot_buf_part, &snapshot);
 
   if (n <= 0) {
     http_500(stream, NULL);
