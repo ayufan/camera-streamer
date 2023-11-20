82dea910bc24756a01f73cbad75df06cdb0b24db
Kamil Trzcinski <ayufan@ayufan.eu>
2023-11-20 21:28:14 +0100

    tools: fix usb_camera.sh not accepting parameters

--- tools/usb_camera.sh
+++ tools/usb_camera.sh
@@ -8,6 +8,6 @@ CAMERA_PATH=( $(echo /dev/v4l/by-id/usb-*-video-index0) )
 set -xeo pipefail
 make -j$(nproc)
 $GDB ./camera-streamer \
-  -camera-path="${CAMERA_PATH[${CAMERA_INDEX:-0}]}" 
+  -camera-path="${CAMERA_PATH[${CAMERA_INDEX:-0}]}" \
   --http-listen=0.0.0.0 \
   "$@"
