78c7b1b78a610c58bf06a1dcce7ff296225d2f73
Patrick Gehrsitz <mryel00.github@gmail.com>
2024-11-30 12:56:41 +0100

    libcamera: add support for ControlTypePoint
    Signed-off-by: Patrick Gehrsitz <mryel00.github@gmail.com>

--- device/libcamera/options.cc
+++ device/libcamera/options.cc
@@ -217,6 +217,11 @@ static int libcamera_device_dump_control_option(device_option_fn fn, void *opaqu
   case libcamera::ControlTypeString:
     opt.type = device_option_type_string;
     break;
+
+  case libcamera::ControlTypePoint:
+    opt.type = device_option_type_float;
+    opt.elems = 2;
+    break;
   }
 
   auto named_values = libcamera_find_control_ids(control_id.id());
@@ -349,6 +354,27 @@ static libcamera::Size libcamera_parse_size(const char *value)
   return libcamera::Size();
 }
 
+static libcamera::Point libcamera_parse_point(const char *value)
+{
+  static const char *POINT_PATTERNS[] =
+  {
+    "(%d,%d)",
+    "%d,%d",
+    NULL
+  };
+
+  for (int i = 0; POINT_PATTERNS[i]; i++) {
+    libcamera::Point point;
+
+    if (2 == sscanf(value, POINT_PATTERNS[i],
+      &point.x, &point.y)) {
+      return point;
+    }
+  }
+
+  return libcamera::Point();
+}
+
 template<typename T, typename F>
 static bool libcamera_parse_control_value(libcamera::ControlValue &control_value, const char *value, const F &fn)
 {
@@ -435,6 +461,11 @@ int libcamera_device_set_option(device_t *dev, const char *keyp, const char *val
       break;
 
     case libcamera::ControlTypeString:
+      break;
+    
+    case libcamera::ControlTypePoint:
+      libcamera_parse_control_value<libcamera::Point>(
+        control_value, value, libcamera_parse_point);
       break;
     }
 
